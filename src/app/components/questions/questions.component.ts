0e18410757315257561e1cddc00e1ca8193369e0
Elishaya <a.lartillier@gmail.com>
2025-01-05 19:36:26 +0100

    work on questions form

--- src/app/components/questions/questions.component.ts
+++ src/app/components/questions/questions.component.ts
@@ -20,31 +20,16 @@ export class QuestionsComponent {
 
   public form: FormGroup; // Représente le formulaire principal
 
-  constructor(private fb: FormBuilder) {
+  constructor(private formBuilder: FormBuilder) {
     // Initialise le formulaire avec un tableau de groupes
-    this.form = this.fb.group({
-      questions: this.fb.array([]),
+    this.form = this.formBuilder.group({
+      questions: this.formBuilder.array([]),
     });
   }
-
-  public serializedQuestions: any[] = [];
 
   ngOnInit(): void {
     console.log('questions recues :', this.questions);
     this.initializeForm();
-    this.serializedQuestions = this.questionControls.map(
-      (control) => control.value
-    );
-    // console.log('questionControls :', this.questionControls);
-    console.log('Serialized questions:', this.serializedQuestions);
-  }
-
-  // Expose les FormGroups dans le FormArray
-  get questionControls() {
-    return (this.form.get('questions') as FormArray).controls as FormGroup[];
-    // const controls = (this.form.get('questions') as FormArray).controls;
-    // console.log('controls :', controls);
-    // return controls;
   }
 
   // Initialise le FormArray avec les questions
@@ -55,104 +40,15 @@ export class QuestionsComponent {
     }
 
     const questionGroups = this.questions.map((question) =>
-      this.fb.group({
+      this.formBuilder.group({
         id: [question.id], // Identifiant de la question
         selectedAnswer: [null], // Réponse sélectionnée
       })
     );
 
-    this.form.setControl('questions', this.fb.array(questionGroups));
+    this.form.setControl('questions', this.formBuilder.array(questionGroups));
   }
-  public onSubmit(): void {
-    console.log('form :', this.form.value);
+  public onSubmitForm(): void {
+    console.log('form réponses :', this.form.value);
   }
-  //   // Soumission du formulaire
-  //   public onSubmit(): void {
-  //     const userResponses = this.form.value.questions;
-
-  //     // Vérifie les réponses et calcule les résultats
-  //     const results = this.questions.map((question, index) => {
-  //       const selectedAnswer = userResponses[index].selectedAnswer;
-  //       const correctAnswer = question.answers.find(
-  //         (a: any) => a.isCorrect
-  //       )?.text;
-
-  //       return {
-  //         question: question.text,
-  //         selectedAnswer,
-  //         isCorrect: selectedAnswer === correctAnswer,
-  //       };
-  //     });
-
-  //     console.log('Résultats du quiz :', results);
-  //   }
 }
-// import { Component, input, Input, OnChanges, OnInit } from '@angular/core';
-// import { Question } from '../../business/models/question.model';
-// import {
-//   FormArray,
-//   FormBuilder,
-//   FormGroup,
-//   ReactiveFormsModule,
-// } from '@angular/forms';
-
-// @Component({
-//   selector: 'app-questions',
-//   standalone: true,
-//   imports: [ReactiveFormsModule],
-//   templateUrl: './questions.component.html',
-//   styleUrl: './questions.component.scss',
-// })
-// export class QuestionsComponent implements OnInit {
-//   // input pour recevoir les questions depuis le parent QuizPage
-//   // public readonly questions = input.required<Question[]>();
-//   @Input() public questions: Question[] = []; // Liste des questions reçues du parent
-
-//   public form: FormGroup; // Représente le formulaire principal
-
-//   constructor(private formBuilder: FormBuilder) {
-//     // Initialisation du formulaire
-//     this.form = this.formBuilder.group({
-//       questions: this.formBuilder.array([]), // Un tableau de groupes (un pour chaque question)
-//     });
-//   }
-
-//   ngOnInit(): void {
-//     this.initializeForm(); // Réinitialise le formulaire lorsque les questions changent
-//   }
-
-//   // Méthode pour exposer les contrôles du FormArray
-//   get questionControls() {
-//     return (this.form.get('questions') as FormArray).controls;
-//   }
-
-//   private initializeForm(): void {
-//     const questionGroups = this.questions.map((question) =>
-//       this.formBuilder.group({
-//         id: question.id, // Identifiant de la question
-//         // text: question.text,
-//         selectedAnswer: null, // Réponse sélectionnée
-//       })
-//     );
-
-//     //Met à jour le formulaire avec les controles des questions
-//     this.form.setControl('questions', this.formBuilder.array(questionGroups));
-//   }
-
-//   public onSubmit(): void {
-//     const userResponses = this.form.value.questions;
-
-//     //Vérification des réponses
-//     const results = this.questions.map((question, index) => {
-//       const selectedAnswer = userResponses[index].selectedAnswer;
-//       const correctAnswer = question.answers.find((answer) => answer.isCorrect);
-
-//       return {
-//         question: question.text,
-//         selectedAnswer,
-//         isCorrect: selectedAnswer === correctAnswer,
-//       };
-//     });
-//     console.log('Résultats :', results);
-//   }
-// }
