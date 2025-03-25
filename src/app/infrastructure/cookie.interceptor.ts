527da1cd310ee278057ea965c8287a0474b23d40
Elishaya <a.lartillier@gmail.com>
2025-03-25 13:38:59 +0100

    refactor : quiz list css with sass and accessilibility

--- src/app/infrastructure/cookie.interceptor.ts
+++ src/app/infrastructure/cookie.interceptor.ts
@@ -1,9 +1,15 @@
 import { HttpInterceptorFn } from "@angular/common/http";
 
 export const cookieInterceptor: HttpInterceptorFn = (request, next) => {
+  console.log('Cookie interceptor');
+  // On n'autorise les requêtes que vers notre serveur
+  // TODO : utiliser les environnements Angular : https://angular.dev/tools/cli/environments
+  if (request.url.startsWith('http://localhost:5000/')) {
     const newRequest = request.clone({
-        withCredentials: true
+      withCredentials: true,
     });
 
     return next(newRequest);
-}
+  }
+  return next(request);
+};
